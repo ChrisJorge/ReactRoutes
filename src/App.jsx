2627502bc70b49b37cd6fe0f819572a4e794d5da
Chris <Christopherjorge47@icloud.com>
2024-05-10 20:11:26 -0400

    Fixed error where refreshing would cause DOM to be manipulated outside of render, causing cannot read properties of undefined error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+
 import "./style.css";
-import {Route, Routes} from "react-router-dom";
-import { useState } from "react";
+import {Route, Routes, useLocation} from "react-router-dom";
+import { useState, useEffect } from "react";
 // Importing Pages
 
 import Homepage from "./pages/Homepage";
@@ -8,51 +9,54 @@ import Collection from "./pages/Collection";
 
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false) ;
-  let moon = document.querySelector('.Moon')
-  let sun = document.querySelector('.Sun')
-  let body = document.querySelector('body')
-  let title = document.querySelector('.homePageTitle')
+  const [darkMode, setDarkMode] = useState(false);
 
-  if(!darkMode)
-    {
-      moon.setAttribute('style', 'display: block')
-      sun.setAttribute('style', 'display: none')
-      body.setAttribute("style", "background-color:white");
-      for(let i = 0; i < document.querySelectorAll('.change').length; i++)
+
+
+  useEffect(() => {
+
+    const update = () => {
+      let moon = document.querySelectorAll('svg')[1]
+      let sun = document.querySelectorAll('svg')[0]
+      let body = document.querySelector('body')
+
+      if(!darkMode)
         {
-          let text = document.querySelectorAll('.change')[i]
-          text.setAttribute("style", "color: black")
+          moon.setAttribute('style', 'display: block')
+          sun.setAttribute('style', 'display: none')
+          body.setAttribute("style", "background-color:white");
+          for(let i = 0; i < document.querySelectorAll('.change').length; i++)
+            {
+              let text = document.querySelectorAll('.change')[i]
+              text.setAttribute("style", "color: black")
+            }
+          for(let i = 0; i < document.querySelectorAll('.changeBorder').length; i++)
+            {
+              let options = document.querySelectorAll('.changeBorder')[i];
+              options.setAttribute('style', "border-color: black")
+            }
         }
-      for(let i = 0; i < document.querySelectorAll('.changeBorder').length; i++)
-        {
-          let options = document.querySelectorAll('.changeBorder')[i];
-          options.setAttribute('style', "border-color: black")
-        }
-      // title.style.color = 'black';
-      // for(let i = 0; i < document.querySelectorAll('.homePageOption').length; i++)
-      //   {
-      //     let text = document.querySelectorAll('.homePageOptionText')[i]
-      //     let options = document.querySelectorAll('.homePageOption')[i]
-      //     text.style.color = 'black';
-      //     options.style.borderColor = 'black';
-      //   }
+      else{
+        moon.setAttribute('style', 'display: none')
+        sun.setAttribute('style', 'display: block')
+        body.setAttribute("style", "background-color:black");
+        for(let i = 0; i < document.querySelectorAll('.change').length; i++)
+          {
+            let text = document.querySelectorAll('.change')[i]
+            text.setAttribute("style", "color: white")
+          }
+        for(let i = 0; i < document.querySelectorAll('.changeBorder').length; i++)
+          {
+            let options = document.querySelectorAll('.changeBorder')[i];
+            options.setAttribute('style', "border-color: white")
+          }
+      }
     }
-  else{
-    moon.setAttribute('style', 'display: none')
-    sun.setAttribute('style', 'display: block')
-    body.setAttribute("style", "background-color:black");
-    for(let i = 0; i < document.querySelectorAll('.change').length; i++)
-      {
-        let text = document.querySelectorAll('.change')[i]
-        text.setAttribute("style", "color: white")
-      }
-    for(let i = 0; i < document.querySelectorAll('.changeBorder').length; i++)
-      {
-        let options = document.querySelectorAll('.changeBorder')[i];
-        options.setAttribute('style', "border-color: white")
-      }
-  }
+
+      update()
+  }, [darkMode])
+
+  
   return (
     <div className="App">
       <Routes>
