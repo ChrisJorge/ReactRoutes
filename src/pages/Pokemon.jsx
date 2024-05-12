b2217cc5a9092945dc6943dcb7c4973ebf55f971
Chris <Christopherjorge47@icloud.com>
2024-05-12 18:09:38 -0400

    Modified output for each pokemon

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -4,18 +4,32 @@ import { useParams } from 'react-router-dom'
 import Nav from '../components/Nav'
 
 function Pokemon({setDarkMode, darkMode, update}) {
-    const params = useParams()
-    const name = params.name
+    const params = useParams();
+    const name = params.name;
     const url = `https://pokeapi.co/api/v2/pokemon/${name}`
    
-    const [pokemon, setPokemon] = useState('null')
-    const [image, setImage] = useState('null')
+    const [pokemon, setPokemon] = useState('null');
+    const [image, setImage] = useState('null');
+    const [type, setType] = useState('null');
+    const [attack, setAttack] = useState('null');
+    const [defense, setDefense] = useState('null');
+    const [speed, setSpeed] = useState('null');
+    const [health, setHealth] = useState('null');
+    const [ weight, setWeight] = useState('null')
+   
 
     const individualPokemon = async () => {
         try{
             const response = await fetch(url);
             const data = await response.json();
+            console.log(data)
             setImage(data.sprites.front_default)
+            setType(data.types[0].type.name)
+            setHealth(data.stats[0].base_stat)
+            setAttack(data.stats[1].base_stat)
+            setDefense(data.stats[2].base_stat)
+            setSpeed(data.stats[5].base_stat)
+            setWeight(data.weight)
             setPokemon(data);
         } catch (e)
         {
@@ -32,9 +46,19 @@ function Pokemon({setDarkMode, darkMode, update}) {
         return(
             <div>
                 <Nav setDarkMode = {setDarkMode} darkMode = {darkMode} update = {update}/>
-                <div className='pokemonContainer'>
-                    <h1 className='change'>{pokemon.name}</h1>
-                     <img src={image} className='Image'/>  
+                <div className="masterContainer">
+                    <div className='pokemonContainer'>
+                        <h1 className='change'>{pokemon.name}</h1>
+                        <img src={image} className='Image'/>
+                        <div className="info">
+                            <p className="infoText change">Type: {type}</p>
+                            <p className="infoText change">HP: {health}</p>
+                            <p className="infoText change">Attack: {attack}</p>
+                            <p className="infoText change">Defense: {defense}</p>
+                            <p className="infoText change">Speed: {speed}</p>
+                            <p className="infoText change">Weight: {weight}lbs</p>
+                        </div> 
+                    </div>
                 </div>
             </div>
         )
