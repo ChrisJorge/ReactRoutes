b3bb0816b582e62062d21ff84d708f7ae21f54c7
Chris <Christopherjorge47@icloud.com>
2024-05-12 18:10:46 -0400

    Modified js

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import Nav from '../components/Nav'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Nav from '../components/Nav';
 
 function Pokemon({setDarkMode, darkMode, update}) {
     const params = useParams();
     const name = params.name;
-    const url = `https://pokeapi.co/api/v2/pokemon/${name}`
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
    
     const [pokemon, setPokemon] = useState('null');
     const [image, setImage] = useState('null');
@@ -15,32 +15,32 @@ function Pokemon({setDarkMode, darkMode, update}) {
     const [defense, setDefense] = useState('null');
     const [speed, setSpeed] = useState('null');
     const [health, setHealth] = useState('null');
-    const [ weight, setWeight] = useState('null')
+    const [ weight, setWeight] = useState('null');
    
 
     const individualPokemon = async () => {
         try{
             const response = await fetch(url);
             const data = await response.json();
-            console.log(data)
-            setImage(data.sprites.front_default)
-            setType(data.types[0].type.name)
-            setHealth(data.stats[0].base_stat)
-            setAttack(data.stats[1].base_stat)
-            setDefense(data.stats[2].base_stat)
-            setSpeed(data.stats[5].base_stat)
-            setWeight(data.weight)
+            console.log(data);
+            setImage(data.sprites.front_default);
+            setType(data.types[0].type.name); 
+            setHealth(data.stats[0].base_stat);
+            setAttack(data.stats[1].base_stat);
+            setDefense(data.stats[2].base_stat);
+            setSpeed(data.stats[5].base_stat);
+            setWeight(data.weight);
             setPokemon(data);
         } catch (e)
         {
-            console.error(e)
-        }
+            console.error(e);
+        };
     };
 
     useEffect(() => 
     {
-        individualPokemon()
-    }, [])
+        individualPokemon();
+    }, []);
   
     const loaded = () => {
         return(
@@ -62,13 +62,13 @@ function Pokemon({setDarkMode, darkMode, update}) {
                 </div>
             </div>
         )
-    }
-
-    const loading = () => {
-        return <h1>Loading...</h1>
     };
 
-    return pokemon ? loaded() : loading()
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return pokemon ? loaded() : loading();
 }
 
-export default Pokemon
+export default Pokemon;
